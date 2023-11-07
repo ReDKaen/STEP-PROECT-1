630f8f74cb3907f3af2fbd6b8f6725b2a4074618
ReDKaen <rigiy.yojik@gmail.com>
2023-11-07 18:13:58 +0200

    add filter

--- script/script.js
+++ script/script.js
@@ -12,7 +12,48 @@ tabs.forEach((tab, index) => {
     });
 });
 
+const list = document.querySelector(".wrraper-button-of-amazing-work");
+const listContent = document.querySelectorAll(".img-of-amazing-work");
+const listItems = document.querySelectorAll(".button-of-amazing-work");
 
+function filter() {
+    list.addEventListener("click", event => {
+        const targetId = event.target.dataset.id
+        const target = event.target
 
+        if (target.classList.contains("button-of-amazing-work")){
+            listItems.forEach(listItem => listItem.classList.remove("active"))
+            target.classList.add("active")
+        }
 
+        switch(targetId){
+            case "all":
+                getContent("img-of-amazing-work")
+                break
+            case "Graphic-Design":
+                getContent(targetId)
+                break 
+            case "Web-Design":
+                getContent(targetId)
+                break
+            case "Landing-Pages":
+                getContent(targetId)
+                break
+            case "Wordpress":
+                getContent(targetId)
+                break
+        }
+    })
+}
 
+filter()
+ 
+function getContent(className) {
+    listContent.forEach(item => {
+        if (item.classList.contains(className)){
+            item.style.display = "block"
+        } else {
+            item.style.display = "none"
+        }
+    })
+}
